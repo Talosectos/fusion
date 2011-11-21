cde2ec22105e78bbcd1151b76eb19b9d00ba5a66
Talosectos <talosectos@gmail.com>
2011-11-21 10:26:57 +0100

    Ajout langpack FR

---
+++ sql/langpack/mangos_locales_achievement_reward.sql
@@ -0,0 +1,14 @@
+﻿SET NAMES utf8;
+INSERT IGNORE INTO `locales_achievement_reward` (`entry`) SELECT `entry` FROM `achievement_reward`;
+
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Félicitations', `text_loc2` = 'Toutes mes félicitations pour votre application à atteindre votre quatre-vingtième saison d\'aventures. Vous êtes indubitablement $gdévoué:dévouée; à la cause de l\'éradication du mal qui nous touche en Azeroth.\n\nEt bien que le voyage n\'ait pas été une mince affaire jusqu\'à présent, la véritable bataille nous attend encore.\n\nContinuez le combat !\n\nRhonin' WHERE `entry` = 13;
+UPDATE `locales_achievement_reward` SET `text_loc2` = 'Eh ben, vous !\n\nEt moi qui croyais avoir tout vu sur ces terres gelées ! Le feu de l\'exploration brûle en vous. Cela saute à mes yeux de nain.\n\nPortez ce tabard avec fierté. De cette façon, vos amis sauront toujours à qui demander leur chemin en temps voulu !\n\nRestez bien en selle !\n\nBrann Barbe-de-bronze' WHERE `entry` = 45;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Pour la Horde !', `text_loc2` = 'En ces temps troublés, nos souffrances engendrent de vrais héros. Vous êtes l\'un d\'eux.\n\nNous sommes en guerre. Vos efforts soutiennent notre cause en Azeroth. Vos hautes actions seront récompensées. Prenez ce prix d\'Orgrimmar et chevauchez vers la gloire.\n\nPour la Horde !\n\nChef de guerre Thrall' WHERE `entry` = 619;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Pour l\'Alliance !' WHERE `entry` = 614;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Pour la Horde !' WHERE `entry` = 619;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Gloire à l\'écumeur de raids de la Couronne de glace' WHERE `entry` = 4603;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Une offrande à l\'immortalité' WHERE `entry` = 4079;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Une offrande à l\'immortalité' WHERE `entry` = 4156;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Niveau 80' WHERE `entry` = 13;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Maître de l\'île des Conquérants' WHERE `entry` = 3857;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Maître de l\'île des Conquérants' WHERE `entry` = 3957;
---
+++ sql/langpack/mangos_transports.sql
@@ -0,0 +1,28 @@
+﻿SET NAMES utf8;
+
+UPDATE `transports` SET `name` = 'Vaisseau de combat de la Horde' WHERE entry = 195276;
+UPDATE `transports` SET `name` = 'Vaisseau de combat de l\'Alliance' WHERE entry = 195121;
+UPDATE `transports` SET `name` = 'La citadelle de Glace (Alliance)' WHERE entry = 201811;
+UPDATE `transports` SET `name` = 'La citadelle de Glace (Horde)' WHERE entry = 201812;
+UPDATE `transports` SET `name` = 'Le Brise-ciel' WHERE entry = 201598;
+UPDATE `transports` SET `name` = 'Le Marteau d\'Orgrim' WHERE entry = 201599;
+UPDATE `transports` SET `name` = 'Raid ICC, Le Marteau d\'Orgrim' WHERE entry = 201581;
+UPDATE `transports` SET `name` = 'Raid ICC, Le Brise-ciel' WHERE entry = 201580;
+UPDATE `transports` SET `name` = 'Zeppelin, Horde (Raid ICC)' WHERE entry = 201834;
+UPDATE `transports` SET `name` = 'Orgrimmar et Les Pitons du Tonnerre' WHERE entry = 190549;
+UPDATE `transports` SET `name` = 'Le Marteau d\'Orgrim' WHERE entry = 192241;
+UPDATE `transports` SET `name` = 'Piste d\'atterrissage de Spumelevier' WHERE entry = 192242;
+UPDATE `transports` SET `name` = 'Bateau Pirate' WHERE entry = 187038;
+UPDATE `transports` SET `name` = 'Orgrimmar et Fossoyeuse' WHERE entry = 164871;
+UPDATE `transports` SET `name` = 'Campement Grom\'Gol et Fossoyeuse' WHERE entry = 176495;
+UPDATE `transports` SET `name` = 'Port de Menethil et Auberdine' WHERE entry = 176310;
+UPDATE `transports` SET `name` = 'Teldrassil et Auberdine' WHERE entry = 176244;
+UPDATE `transports` SET `name` = 'Port de Menethil et L\'île de Theramore' WHERE entry = 176231;
+UPDATE `transports` SET `name` = 'Campement Grom\'Gol et Orgrimmar' WHERE entry = 175080;
+UPDATE `transports` SET `name` = 'Cabestan et Baie-du-butin' WHERE entry = 20808;
+UPDATE `transports` SET `name` = 'La côte oubliée et l\'île de sardor' WHERE entry = 177233;
+UPDATE `transports` SET `name` = 'île de brume-azur et Auberdine' WHERE entry = 181646;
+UPDATE `transports` SET `name` = 'Valgarde et Port de Menethil' WHERE entry = 181688;
+UPDATE `transports` SET `name` = 'Fossoyeuse et Accostage de la Vengeance' WHERE entry = 181689;
+UPDATE `transports` SET `name` = 'Orgrimmar et Bastion Chanteguerre' WHERE entry = 186238;
+UPDATE `transports` SET `name` = 'Porte d\'Hurlevent et Donjon de la bravoure, Toundra Boréenne ("Le Kraken") ' WHERE entry = 190536;
