a0e1194ddb8a7874ede1a0c8eb2d1ea3e7c49170
Talosectos <talosectos@gmail.com>
2011-11-21 10:05:21 +0100

    Comment in sql files

--- sql/dk_quest.sql
+++ sql/dk_quest.sql
@@ -1,4 +1,7 @@
 /*
 -- Debug des quêtes de la zone de départ dk
 */
+
+-- Debug de la quete Death Comes From On High: Completion automatique (l'oeil de marche pas)
+-- Rajout de deux points de talents manquants pour arriver à 71 points au niveau 80
 UPDATE `quest_template` SET `Method`=0, `BonusTalents`=2 WHERE `entry`='12641';
