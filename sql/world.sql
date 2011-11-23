c9541e0e42ab4f5a93e1e25ea88bf7e5df2f96ce
Talosectos <talosectos@gmail.com>
2011-11-23 14:25:30 +0100

    Fix Training Dummy Move

--- sql/world.sql
+++ sql/world.sql
@@ -3,3 +3,10 @@
 */
 
 UPDATE `creature_template` SET `AIName`='' WHERE `entry`='20159';
+
+-- Disable Training Dummy Move
+
+UPDATE `dev_mangos`.`creature_template` SET `AIName`='NullAI' WHERE `entry`='31146';
+UPDATE `dev_mangos`.`creature_template` SET `AIName`='NullAI' WHERE `entry`='32667';
+UPDATE `dev_mangos`.`creature_template` SET `AIName`='NullAI' WHERE `entry`='32666';
+UPDATE `dev_mangos`.`creature_template` SET `AIName`='NullAI' WHERE `entry`='31144';
