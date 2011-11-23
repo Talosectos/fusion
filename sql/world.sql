ca32f7d83e42a12c7bcc2472098bb16a3a749fc9
Talosectos <talosectos@gmail.com>
2011-11-23 14:36:07 +0100

    Fix Argent Tournament Training Dummy Move

--- sql/world.sql
+++ sql/world.sql
@@ -6,7 +6,10 @@ UPDATE `creature_template` SET `AIName`='' WHERE `entry`='20159';
 
 -- Disable Training Dummy Move
 
-UPDATE `dev_mangos`.`creature_template` SET `AIName`='NullAI' WHERE `entry`='31146';
-UPDATE `dev_mangos`.`creature_template` SET `AIName`='NullAI' WHERE `entry`='32667';
-UPDATE `dev_mangos`.`creature_template` SET `AIName`='NullAI' WHERE `entry`='32666';
-UPDATE `dev_mangos`.`creature_template` SET `AIName`='NullAI' WHERE `entry`='31144';
+UPDATE `creature_template` SET `AIName`='NullAI' WHERE `entry`='31146';
+UPDATE `creature_template` SET `AIName`='NullAI' WHERE `entry`='32667';
+UPDATE `creature_template` SET `AIName`='NullAI' WHERE `entry`='32666';
+UPDATE `creature_template` SET `AIName`='NullAI' WHERE `entry`='31144';
+UPDATE `creature_template` SET `AIName`='NullAI' WHERE `entry`='33243';
+UPDATE `creature_template` SET `AIName`='NullAI' WHERE `entry`='33272';
+UPDATE `creature_template` SET `AIName`='NullAI' WHERE `entry`='33229';
