101fc51ab666363482d68b95e51930262fe497c6
Talosectos <talosectos@gmail.com>
2011-11-25 15:33:59 +0100

    Fix wintergasp quest

---
+++ sql/northrend/wintergrasp/quest.sql
@@ -0,0 +1,21 @@
+-- Update des quêtes du Joug d'hiver
+
+-- H2
+UPDATE `quest_template` SET `ExclusiveGroup`=13193 WHERE `entry`='13193';
+UPDATE `quest_template` SET `ExclusiveGroup`=13193 WHERE `entry`='13199';
+UPDATE `quest_template` SET `ExclusiveGroup`=13202 WHERE `entry`='13192';
+UPDATE `quest_template` SET `ExclusiveGroup`=13202 WHERE `entry`='13202';
+UPDATE `quest_template` SET `ExclusiveGroup`=13194 WHERE `entry`='13194';
+UPDATE `quest_template` SET `ExclusiveGroup`=13194 WHERE `entry`='13201';
+UPDATE `quest_template` SET `ExclusiveGroup`=13200 WHERE `entry`='13200';
+UPDATE `quest_template` SET `ExclusiveGroup`=13200 WHERE `entry`='13191';
+
+-- A2
+UPDATE `mangos`.`quest_template` SET `ExclusiveGroup`=13197 WHERE `entry`='13197';
+UPDATE `mangos`.`quest_template` SET `ExclusiveGroup`=13197 WHERE `entry`='236';
+UPDATE `mangos`.`quest_template` SET `ExclusiveGroup`=13154 WHERE `entry`='13154';
+UPDATE `mangos`.`quest_template` SET `ExclusiveGroup`=13154 WHERE `entry`='13196';
+UPDATE `mangos`.`quest_template` SET `ExclusiveGroup`=13153 WHERE `entry`='13153';
+UPDATE `mangos`.`quest_template` SET `ExclusiveGroup`=13153 WHERE `entry`='13198';
+UPDATE `mangos`.`quest_template` SET `ExclusiveGroup`=13156 WHERE `entry`='13156';
+UPDATE `mangos`.`quest_template` SET `ExclusiveGroup`=13156 WHERE `entry`='13195';
