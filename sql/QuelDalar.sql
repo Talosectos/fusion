fbf4e4a148c2b58b16de8c0cbf7ddbb90de54be5
Talosectos <talosectos@gmail.com>
2011-11-25 09:35:00 +0100

    Modify creature_template Quel Dalar

--- sql/QuelDalar.sql
+++ sql/QuelDalar.sql
@@ -1,11 +1,13 @@
 -- Debug de la suite de quete de Quel'Dalar
 
 -- Retour Arrière
-UPDATE `dev_mangos`.`creature_template` SET `faction_A`=2027, `faction_H`=2027, `unit_flags`=256 WHERE `entry`='36774';
+-- UPDATE `creature_template` SET `faction_A`=2027, `faction_H`=2027, `unit_flags`=256 WHERE `entry`='36774';
 
 
 -- Debug de la quete Les plans du Concordat agenté
 -- Alliance
 -- Horde
-UPDATE `dev_mangos`.`creature_template` SET `faction_A`=7, `faction_H`=7, `unit_flags`=0 WHERE `entry`='36774';
+UPDATE `creature_template` SET `faction_A`=7, `faction_H`=7, `unit_flags`=0 WHERE `entry`='36774';
 
+-- Modification de Quel'Dalar
+UPDATE `creature_template` SET `modelid_1`=25398, `modelid_2`=0, `mindmg`=586, `maxdmg`=741, `attackpower`=895, `dmg_multiplier`=7, `ScriptName`='' WHERE `entry`='37158';
