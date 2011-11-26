93e0c45b5dd14c8cb39988f02d7ce98bad0f1ccc
Talosectos <talosectos@gmail.com>
2011-11-26 10:44:34 +0100

    Fix Deathwhisper's add IA

--- sql/northrend/icecrown_citadel/icecrown_citadel/icecrown_citadel.sql
+++ sql/northrend/icecrown_citadel/icecrown_citadel/icecrown_citadel.sql
@@ -81,4 +81,5 @@ INSERT IGNORE INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modeli
 
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES
 -- Adds Dame Murmemort Phase 2
-(3822201,38222,0,2,100,30,0,0,0,0,11,71494,0,0,0,0,0,0,0,0,0,0, 'Vengeful Shade - Vengeful Blast');
+(3822201,38222,0,0,100,30,0,0,0,0,11,71494,0,0,0,0,0,0,0,0,0,0, 'Vengeful Shade - Vengeful Blast'),
+(3822202,38222,0,0,100,30,0,0,0,0,41,10000,0,0,0,0,0,0,0,0,0,0, 'Vengeful Shade - Despawn');
